6e7b138e0c8b6d15117487db17c5160e96e940e5
l-nikola <legay.nikola.pro@gmail.com>
2025-09-04 13:13:25 +0200

    feat : install react router dom + create route

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./styles/index.scss";
-import Home from "./pages/Home.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
