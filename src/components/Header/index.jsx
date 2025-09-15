0bd008a3725ae08867a4445da3c06d20a33e5f5e
l-nikola <legay.nikola.pro@gmail.com>
2025-09-15 12:48:18 +0200

    feat : header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,31 @@
 export default function Header() {
   return (
-    <nav className="header">
+    <header>
       <a href="/">
-        <img src="public/images/argentBankLogo.png" alt="Argent Bank Logo" />
+        <img src="/images/argentBankLogo.png" alt="Argent Bank Logo" />
       </a>
-      <div className="header__profile">
-        <a href="./sign-in.html">
-          <i className="fa fa-user-circle"></i>
-          Name
-        </a>
-      </div>
-      <div className="header__login">
-        <a href="./sign-in.html">
-          <i class="fa-solid fa-arrow-right-to-bracket"></i>
-          Sign In
-        </a>
-      </div>
-    </nav>
+      <nav>
+        <ul>
+          <li>
+            <a href="./sign-in.html">
+              <i className="fa-solid fa-user"></i>
+              Name
+            </a>
+          </li>
+          <li>
+            <a href="./sign-in.html">
+              <i className="fa-solid fa-arrow-right-to-bracket"></i>
+              Sign In
+            </a>
+          </li>
+          {/* <li>
+          <a href="./sign-in.html">
+          <i className="fa-solid fa-arrow-right-from-bracket"></i>
+          Sign Out
+          </a>
+          </li> */}
+        </ul>
+      </nav>
+    </header>
   );
 }
