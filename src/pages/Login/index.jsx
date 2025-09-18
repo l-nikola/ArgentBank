38fa74ae21fd979acfdca85a546ac25511791f17
l-nikola <legay.nikola.pro@gmail.com>
2025-09-18 10:14:27 +0200

    feat : login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,31 +1,23 @@
 export default function Login() {
   return (
-    // <main class="main bg-dark">
     <main className="login">
-      <section class="sign-in-content">
-        <i class="fa fa-user-circle sign-in-icon"></i>
-        <h1>Sign In</h1>
-        <form>
-          <div class="input-wrapper">
-            <label for="username">Username</label>
-            <input type="text" id="username" />
-          </div>
-          <div class="input-wrapper">
-            <label for="password">Password</label>
-            <input type="password" id="password" />
-          </div>
-          <div class="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
-          </div>
-
-          <a href="./user.html" class="sign-in-button">
-            Sign In
-          </a>
-
-          <button class="sign-in-button">Sign In</button>
-        </form>
-      </section>
+      <i className="fa fa-user-circle"></i>
+      <h1>Sign In</h1>
+      <form>
+        <div className="wrapper">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+        <div className="wrapper">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="remember">
+          <input type="checkbox" id="remember-me" />
+          <label htmlFor="remember-me">Remember me</label>
+        </div>
+        <button>Sign In</button>
+      </form>
     </main>
   );
 }
