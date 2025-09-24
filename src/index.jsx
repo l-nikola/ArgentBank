14f7d9e4aaac56c1762e008980c48c92663cbed2
l-nikola <legay.nikola.pro@gmail.com>
2025-09-24 21:43:10 +0200

    feat : configure store

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import "./styles/index.scss";
 import Login from "./pages/Login";
 import Home from "./pages/Home/";
@@ -10,15 +12,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/transaction" element={<Transaction />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
-    <Footer />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </Provider>
 );
